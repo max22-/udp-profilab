82a9a1ec97870e7295473cf7361b3809fbcba835
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2021-11-17 09:58:32 +0000

    Implemented ip address configuration

--- dllmain.cpp
+++ dllmain.cpp
@@ -14,11 +14,16 @@ extern "C" {
 
 const unsigned char inputs = 16;
 const unsigned char outputs = 16;
-const char *HOST = "192.168.4.1";
 const unsigned short PORT = 1234;
-const unsigned char PUSER_INPUT = 0;
-const unsigned char PUSER_OUTPUT = 1;
-const unsigned char PUSER_TIMESTAMP = 2;
+enum {
+  PUSER_INPUT,
+  PUSER_OUTPUT,
+  PUSER_TIMESTAMP,
+  PUSER_IP0,
+  PUSER_IP1,
+  PUSER_IP2,
+  PUSER_IP3
+};
 double SEND_INTERVAL = 5; // seconds
 SOCKET s;
 struct sockaddr_in sin;
@@ -30,6 +35,8 @@ const unsigned char RST = 1;
 const unsigned char CLK_OLD = 0;
 const unsigned char RST_OLD = 1;
 const unsigned char COUNT = 2;
+
+#define STRING_IP(string, puser) { sprintf(string, "%d.%d.%d.%d", (int)puser[PUSER_IP0], (int)puser[PUSER_IP1], (int)puser[PUSER_IP2], (int)puser[PUSER_IP3]); }
 
 void debug(const char *fmt, ...)
 {
@@ -89,26 +96,29 @@ DLLEXPORT void _stdcall GetOutputName(unsigned char channel, unsigned char *name
 
 DLLEXPORT void _stdcall CSimStart(double *PInput, double *POutput, double *PUser)
 {
-	WSADATA wsa;
-
-	WSAStartup(MAKEWORD(2,2),&wsa);
-    s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-	memset((char *) &sin, 0, sizeof(sin));
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(PORT);
-	sin.sin_addr.S_un.S_addr = inet_addr(HOST);
-	
-	PUser[PUSER_INPUT] = 0;
-	PUser[PUSER_OUTPUT] = 0;
-
-    for(int i = 0; i < outputs; i++)
-        POutput[i] = 0;
-    
-    
-    time_t timestamp;
-    time(&timestamp);
-    PUser[PUSER_TIMESTAMP] = (double)timestamp;
+  WSADATA wsa;
+  char host_ip[256];
+  
+  STRING_IP(host_ip, PUser);
+  
+  WSAStartup(MAKEWORD(2,2),&wsa);
+  s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  
+  memset((char *) &sin, 0, sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(PORT);
+  sin.sin_addr.S_un.S_addr = inet_addr(host_ip);
+  
+  PUser[PUSER_INPUT] = 0;
+  PUser[PUSER_OUTPUT] = 0;
+  
+  for(int i = 0; i < outputs; i++)
+    POutput[i] = 0;
+  
+  
+  time_t timestamp;
+  time(&timestamp);
+  PUser[PUSER_TIMESTAMP] = (double)timestamp;
 }
 
 
@@ -146,23 +156,49 @@ DLLEXPORT void _stdcall CCalculate(double *PInput, double *POutput, double *PUse
 
 DLLEXPORT void _stdcall CSimStop(double *PInput, double *POutput, double *PUser)
 {
-    closesocket(s);
-	WSACleanup();
+  closesocket(s);
+  WSACleanup();
 }
 int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szComdLine, int iCmdShow);
+
+static UITextbox *textbox;
+
+int SaveIP(UIElement *button, UIMessage message, int di, void *dp) {
+  int ip[4];
+  double *PUser = (double*)button->cp;
+  
+  if (message == UI_MSG_CLICKED) {
+    sscanf(textbox->string, "%d.%d.%d.%d", &ip[0], &ip[1], &ip[2], &ip[3]);
+    PUser[PUSER_IP0] = ip[0];
+    PUser[PUSER_IP1] = ip[1];
+    PUser[PUSER_IP2] = ip[2];
+    PUser[PUSER_IP3] = ip[3];
+    //debug("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+  }
+  
+  return 0;
+}
 
 DLLEXPORT void _stdcall CConfigure(double *PUser)
 {
   UILabel *label;
-  UITextbox *textbox;
   UIButton *button;
-          
+  char buffer[256];
+
+         
   UIInitialise();
-  UIWindow *window = UIWindowCreate(0, 0, "Configuration", 320, 240);
+  
+  UIWindow *window = UIWindowCreate(0, 0, "Configuration", 320, 170);
   UIPanel *panel = UIPanelCreate(&window->e, UI_PANEL_GRAY | UI_PANEL_MEDIUM_SPACING);
-  label = UILabelCreate(&panel->e, 0, "Adresse IP :", -1);
+  label = UILabelCreate(&panel->e, 0, "Adresse IP ESP32 :", -1);
   textbox = UITextboxCreate(&panel->e, 0);
-  button = UIButtonCreate(&panel->e, 0, "Ok", -1);
+
+  STRING_IP(buffer, PUser);
+  UITextboxClear(textbox, false);
+  UITextboxReplace(textbox, buffer, strlen(buffer), false);
+  button = UIButtonCreate(&panel->e, 0, "Enregistrer", -1);
+  button->e.messageUser = SaveIP;
+  button->e.cp = PUser;
   UIMessageLoop();
   DestroyWindow(window->hwnd);
 }
